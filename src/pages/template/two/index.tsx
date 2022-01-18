5194ea5a86610537d6877cbe374fa18f15fbb906
qhchen <qhchen@secusoft.cc>
2022-01-18 14:22:12 +0800

    feat: init

---
+++ src/pages/template/two/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ContentWrapper from '@/components/ContentWrapper';
+import styles from './index.less';
+
+export default function Page() {
+  return <ContentWrapper title="模版页面2">模版页面2</ContentWrapper>;
+}
